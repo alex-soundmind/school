ce4d32559889a797e9f9b38a1f517d96816bfd2e
alex-soundmind <146514354+alex-soundmind@users.noreply.github.com>
2025-11-09 11:06:05 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ $db   = 'school';
 $host = 'dpg-d4847mbipnbc73d8hlm0-a.singapore-postgres.render.com';
 $user = 'user';
 $pass = 'RFa4bfOpswyRFBK3cZvaU9okEORsFxYO';
+$dsn = "pgsql:host=$host;dbname=$db";
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
